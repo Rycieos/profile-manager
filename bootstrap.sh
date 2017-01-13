8fb29fed79600e9dfa586b3d8db51dd646bfa28b
Mark Vander Stel <mvndrstl@gmail.com>
2017-01-13 15:09:05 -0500

    Add boostrap, fix find, and read config only once

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec bash <(curl https://raw.githubusercontent.com/Rycieos/profile-manager/master/profile-manager) -c <(curl https://raw.githubusercontent.com/Rycieos/dot-files/master/profile-manager/config)
