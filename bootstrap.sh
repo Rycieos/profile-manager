94427495f3fe0e4ba0bb1f625a1515977c0bfa9a
Mark Vander Stel <mvndrstl@gmail.com>
2017-01-13 15:51:35 -0500

    Make bootstrap shorter, add smart checks
    Remove stupid parts of bootstrap.
    Check if git is installed.
    Make repo_dir if it doesn't exist.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-exec bash <(curl https://raw.githubusercontent.com/Rycieos/profile-manager/master/profile-manager) -c <(curl https://raw.githubusercontent.com/Rycieos/dot-files/master/profile-manager/config)
+exec bash <(curl -s https://raw.githubusercontent.com/Rycieos/profile-manager/master/profile-manager) -c <(curl -s https://raw.githubusercontent.com/Rycieos/dot-files/master/profile-manager/config)
